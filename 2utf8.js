aa9526458349bb833a7317bea68648b48fa51a02
Josep Subils <josep@spellbook.io>
2017-10-11 12:00:56 +0200

    [fix] check empty values

--- 2utf8.js
+++ 2utf8.js
@@ -5,12 +5,12 @@ const jchardet = require('jschardet')
 const Iconv = require('iconv').Iconv
 
 module.exports = (text, html_headers) => {
-  if (tck.isEmpty(text)) {
+  if (!tck.isEmpty(text)) {
     var enc
-    if (tck.isEmpty(html_headers)) enc = charset(html_headers, text)
-    if (!tck.isEmpty(enc)) enc = icu.detectCharset(new Buffer(text, 'binary')).toString()
-    if (!tck.isEmpty(enc)) enc = jchardet.detect(text).encoding.toLowerCase()
-    if (!tck.isEmpty(enc)) {
+    if (!tck.isEmpty(html_headers)) enc = charset(html_headers, text)
+    if (tck.isEmpty(enc)) enc = icu.detectCharset(new Buffer(text, 'binary')).toString()
+    if (tck.isEmpty(enc)) enc = jchardet.detect(text).encoding.toLowerCase()
+    if (tck.isEmpty(enc)) {
       return text.toString('utf-8')
     } else {
       try {
